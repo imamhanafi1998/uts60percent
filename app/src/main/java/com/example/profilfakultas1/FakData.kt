f250f5b9c8364756a301fdbb52ab5f7f005265a9
Imam Hanafi <h.imam1998@gmail.com>
2020-03-24 11:14:07 +0700

    initialization + added new components (webView and mailTo)

---
+++ app/src/main/java/com/example/profilfakultas1/FakData.kt
@@ -0,0 +1,5 @@
+package com.example.profilfakultas1
+
+import android.provider.ContactsContract
+
+data class FakData(val namaFak: String, val descFak: String, val fotoFak: Int, val web: String, val email: String)
